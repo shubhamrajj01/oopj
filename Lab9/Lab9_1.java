abf75b4f809b4d6bb7eb76582848a2fcf44e62ed
Shubham <shubhraj510@gmail.com>
2024-03-12 22:39:43 +0530

    new programs added

---
+++ Lab9/Lab9_1.java
@@ -0,0 +1,27 @@
+// Write a Java program to generate an ArrayIndexOutofBoundsException and handle it using catch statement.
+// Input: Enter the numbers -Example: 1 2 3 4 5 ( Suppose array size is 4 )
+// Output: Exception in thread “main”
+// java.lang.ArrayIndexOutOfBoundsException:4
+
+
+import java.util.Scanner;
+
+public class Lab9_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the number");
+        int size = scanner.nextInt();
+        System.out.println("Enter the numbers:");
+        String[] input = scanner.nextLine().split(" ");
+        int[] numbers = new int[size]; // array size is 4
+
+        try {
+            for (int i = 0; i < input.length; i++) {
+                numbers[i] = Integer.parseInt(input[i]);
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception in thread \"main\" java.lang.ArrayIndexOutOfBoundsException: " + e.getMessage());
+        }
+}
+}
---
+++ Lab9/Lab9_2.java
@@ -0,0 +1,30 @@
+// Write a Java program to handle an ArithmeticException
+// using try, catch, and finally block.
+// Input: Operand values for division operation mentioned in the program
+// Output: ArithmeticException caught by try-catch-finally block
+
+package Lab9;
+
+import java.util.Scanner;
+
+public class Lab9_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Enter the numerator: ");
+            int numerator = scanner.nextInt();
+
+            System.out.print("Enter the denominator: ");
+            int denominator = scanner.nextInt();
+
+            int result = numerator / denominator;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException caught by try-catch-finally block");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+
+        scanner.close();
+    }
+}
